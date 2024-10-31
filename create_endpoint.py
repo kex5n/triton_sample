e093583857d291736d4bed76482da36e0bff1651
kex5n <kentaro.ishii0407@gmail.com>
2024-10-31 17:35:36 +0900

    fix

--- create_endpoint.py
+++ create_endpoint.py
@@ -6,18 +6,36 @@ instance_type = 'ml.g4dn.xlarge'
 
 container = f"763104351884.dkr.ecr.{aws_region}.amazonaws.com/sagemaker-tritonserver:23.03-py3"
 
-# Boto3のSageMakerクライアントを取得する
 sagemaker_client = boto3.client('sagemaker', region_name=aws_region)
 
-# 作成するSageMakerモデルの名前（任意）
-model_name = 'pytorch-predictor-onnx-model'
+model_name = 'onnx-predictor-model'
 
-# SageMakerモデルを作成する
 create_model_response = sagemaker_client.create_model(
     ModelName = model_name,
-    ExecutionRoleArn = '<SageMakerの実行ロールのARN>',
+    ExecutionRoleArn = 'arn:aws:iam::026896639530:role/sagemaker',
     PrimaryContainer = {
         'Image': container,
-        'ModelDataUrl': '<S3にアップロードしたモデルアーティファクトのURL>',
+        'ModelDataUrl': 's3://test-kex5n-2/model-onnx.tar.gz',
     }
+)
+
+endpoint_config_name = 'onnx-predictor-endpoint-config'                         
+
+sagemaker_client.create_endpoint_config(
+    EndpointConfigName=endpoint_config_name,
+    ProductionVariants=[
+        {
+            'VariantName': 'variant1',
+            'ModelName': model_name,
+            'InstanceType': instance_type,
+            'InitialInstanceCount': 1
+        }
+    ]
+)
+
+endpoint_name = 'onnx-predictor-endpoint'
+
+create_endpoint_response = sagemaker_client.create_endpoint(
+    EndpointName=endpoint_name,
+    EndpointConfigName=endpoint_config_name
 )
